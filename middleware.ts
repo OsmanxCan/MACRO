febd8eb681ce6f7213769271de0334ed8adb343b
Can <osmancanbozo@gmail.com>
2026-01-13 23:56:53 +0300

    middleware1.2.1

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,31 @@
-// import { NextResponse } from "next/server"
-// import type { NextRequest } from "next/server"
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
-// export function middleware(req: NextRequest) {
-//   const { pathname } = req.nextUrl
+export function middleware(req: NextRequest) {
+  // const { pathname } = req.nextUrl
 
-//   // 🔓 serbest sayfalar
-//   if (
-//     pathname === "/admin/login" ||
-//     pathname === "/admin/set-password"
-//   ) {
-//     return NextResponse.next()
-//   }
+  // // 🔓 serbest sayfalar
+  // if (
+  //   pathname === "/admin/login" ||
+  //   pathname === "/admin/set-password"
+  // ) {
+  //   return NextResponse.next()
+  // }
 
-//   // ⚠️ sadece cookie VAR MI kontrol et (auth yapma)
-//   const hasSession =
-//     req.cookies.get("sb-access-token") ||
-//     req.cookies.get("sb-refresh-token")
+  // // ⚠️ sadece cookie VAR MI kontrol et (auth yapma)
+  // const hasSession =
+  //   req.cookies.get("sb-access-token") ||
+  //   req.cookies.get("sb-refresh-token")
 
-//   if (!hasSession) {
-//     const url = req.nextUrl.clone()
-//     url.pathname = "/admin/login"
-//     return NextResponse.redirect(url)
-//   }
+  // if (!hasSession) {
+  //   const url = req.nextUrl.clone()
+  //   url.pathname = "/admin/login"
+  //   return NextResponse.redirect(url)
+  // }
 
-//   return NextResponse.next()
-// }
+  // return NextResponse.next()
+}
 
-// export const config = {
-//   matcher: ["/admin/:path*"],
-// }
+export const config = {
+  // matcher: ["/admin/:path*"],
+}
