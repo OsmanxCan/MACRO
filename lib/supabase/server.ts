9cca535fe07144232b88f6d726f16c24a1da31aa
Can <osmancanbozo@gmail.com>
2026-01-14 00:26:24 +0300

    server_ts_1

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -6,16 +6,14 @@ export async function createSupabaseServer() {
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
           return cookieStore.getAll()
         },
         setAll(cookies) {
-          cookies.forEach((cookie) => {
-            cookieStore.set(cookie)
-          })
+          //
         },
       },
     }
